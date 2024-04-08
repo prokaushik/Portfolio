026e1b5ead4f2a592105ca0b7d86d158ba662d66
prince <prince.dubia03@gmail.com>
2024-04-08 11:41:45 +0530

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".typing-3", {
+    strings: ["rtfolio", "Kaushik"],
+    typeSpeed: 200,
+    backSpeed: 100,
+    loop: true
+});
+var typed = new Typed(".typing", {
+    strings: ["Web-Developer.", "Software-Developer.", "Web-Designer.", "Freelancer."],
+    typeSpeed: 120,
+    backSpeed: 100,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["Web-Developer.", "Software-Developer.", "Web-Designer.", "Freelancer."],
+    typeSpeed: 120,
+    backSpeed: 100,
+    loop: true
+});
